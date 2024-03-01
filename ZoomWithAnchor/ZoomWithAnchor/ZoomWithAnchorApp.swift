f49f46ccddd08e849ccac69b3df5f52f0e67a765
Dan Waltin <dan@waltin.se>
2024-03-01 14:44:24 +0100

    Added project

---
+++ ZoomWithAnchor/ZoomWithAnchor/ZoomWithAnchorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZoomWithAnchorApp.swift
+//  ZoomWithAnchor
+//
+//  Created by Dan Waltin on 2024-02-29.
+//
+
+import SwiftUI
+
+@main
+struct ZoomWithAnchorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
