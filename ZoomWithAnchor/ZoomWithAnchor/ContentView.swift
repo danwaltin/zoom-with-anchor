e40261fb8865f7a92d38272ef7aa1114e11f545b
Dan Waltin <dan@waltin.se>
2024-03-02 08:24:37 +0100

    Refactored

--- ZoomWithAnchor/ZoomWithAnchor/ContentView.swift
+++ ZoomWithAnchor/ZoomWithAnchor/ContentView.swift
@@ -56,63 +56,6 @@ struct ContentView: View {
 	}
 }
 
-struct ScrollingViewPort: View {
-	let contentWidth: CGFloat
-	let scrollOffset: CGFloat
-	let settings: Settings
-	
-	var body: some View {
-		ZStack {
-			Ruler(
-				numberOfSegments: 10,
-				color: .green,
-				width: contentWidth,
-				height: settings.contentHeight)
-			.border(.gray)
-			.offset(.init(width: rulerOffset, height: 0))
-			viewPort()
-		}
-		.frame(width: 2 * max(settings.viewPortVisibleWidth, contentWidth))
-	}
-	
-	private var rulerOffset: CGFloat {
-		contentWidth / 2 - settings.viewPortVisibleWidth / 2 - scrollOffset
-	}
-	
-	@ViewBuilder
-	private func viewPort() -> some View {
-		HStack(spacing: 0) {
-			viewPortSegment(
-				width: settings.maxContentWidth - settings.viewPortVisibleWidth,
-				height: settings.viewPortHeight,
-				opacity: 0.6)
-			
-			viewPortSegment(
-				width: settings.viewPortVisibleWidth,
-				height: settings.viewPortHeight,
-				opacity: 0.0)
-				.border(Color(white: 0.8))
-			
-			viewPortSegment(
-				width: settings.maxContentWidth - settings.viewPortVisibleWidth,
-				height: settings.viewPortHeight,
-				opacity: 0.6)
-		}
-	}
-	
-	@ViewBuilder
-	private func viewPortSegment(width: CGFloat, height: CGFloat, opacity: Double) -> some View {
-		Path()
-			.frame(width: max(0, width), height: height)
-			.background(
-				Rectangle()
-					.fill(.background)
-					.opacity(opacity)
-			)
-	}
-	
-}
-
 #Preview {
 	ContentView()
 }
